6fcd6200939ff068d629271e5640bf6689a85682
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-02-19 01:17:00 -0300

    Lista de Tareas

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import academloLogo from './imagenes/logo_academlo.png'
+import ListaDeTareas from './components/ListaDeTareas';
+
+
+
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className='academlo-logo-contenedor'>
+       <img 
+        src={academloLogo}
+        className='academlo-logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+      <h1>Mis Tareas</h1>
+      <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
