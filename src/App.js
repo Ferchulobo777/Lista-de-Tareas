c0449a875f6737eb53c155892b2173090bcf9696
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-02-19 01:33:19 -0300

    update

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
   return (
     <div className="aplicacion-tareas">
       <div className='academlo-logo-contenedor'>
-       <img 
-        src={academloLogo}
-        className='academlo-logo' />
+       <img src={academloLogo} className='academlo-logo' alt='text' />
       </div>
       <div className='tareas-lista-principal'>
       <h1>Mis Tareas</h1>
